3dbebb64258bb07013355612e5594960716984fb
Christoph Gruener <christophgruener@me.com>
2019-02-13 18:36:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   array_as_string = ""
   array.each do |word|
-    word != array[-2] ? array_as_string << word + ", " : array_as_string << 
-    
+    word != array[-1] ? array_as_string << word + ", " : array_as_string << " and " + word
+  end
+  array_as_string  
 end
