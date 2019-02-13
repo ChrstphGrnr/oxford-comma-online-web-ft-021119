e0b900060aa2fd94755491a031ac37ceb89735b7
Christoph Gruener <christophgruener@me.com>
2019-02-13 18:40:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     array.join(" and ")
   else
       array.each do |word|
-      word != array[-1] ? array_as_string << word + ", " : array_as_string << " and " + word
+      word != array[-1] ? array_as_string << word + ", " : array_as_string << "and " + word
     end
     array_as_string 
   end
