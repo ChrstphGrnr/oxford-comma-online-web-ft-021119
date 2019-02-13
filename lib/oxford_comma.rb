3e52b64aebf27fe1fb82e11a24fd207344eed0f3
Christoph Gruener <christophgruener@me.com>
2019-02-13 18:39:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   array_as_string = ""
   if array.length == 1
     array.join
-  elsif array.length == 1 
+  elsif array.length == 2 
     array.join(" and ")
   else
       array.each do |word|
