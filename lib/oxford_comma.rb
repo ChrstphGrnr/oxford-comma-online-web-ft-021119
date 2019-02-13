0f230030ed42375dc3c8b5be1605f1bb67a5a961
Christoph Gruener <christophgruener@me.com>
2019-02-13 18:40:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  array_as_string = ""
+  if array.length == 1
+    array.join
+  elsif array.length == 2 
+    array.join(" and ")
+  else
+      array.each do |word|
+      word != array[-1] ? array_as_string << word + ", " : array_as_string << "and " + word
+    end
+    array_as_string 
+  end
 end
